769293762f61a5d2873a1cf59a48731ab1dfd12b
SnehAuxi <sneh@auxi.ai>
2020-11-19 11:39:52 -0500

    Deleted locallibrary folder

--- locallibrary/catalog/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-from django.urls import include
-
-
-urlpatterns = [
-    path('', views.index, name='index'),
-    path('books/', views.BookListView.as_view(), name='books'),
-]
